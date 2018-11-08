6e4d695ded95e46c6f8e9289d551e511bbcd4d8c
cseyha <cseyhakh@yahoo.com>
2018-11-08 14:45:26 +0700

    Modified Product Details

--- app/Http/Resources/Product/ProductResource.php
+++ app/Http/Resources/Product/ProductResource.php
@@ -19,8 +19,18 @@ class ProductResource extends JsonResource
             'name' => $this->name,
             'description' => $this->detail,
             'price' => $this->price,
-            'stock' => $this->stock,
-            'discount' => $this->discount
+            'stock' => $this->stock == 0 ? 'Out of Stock' : $this->stock,
+            'discount' => $this->discount,
+            // 17/100 = .17
+            // 1 - .17 = .83
+            // .83 * 404
+            'totalPrice' => round((1 - ($this->discount/100)) * $this->price,2),
+            //app/Model/Product.php/public function reviews
+            'rating' => $this->reviews->count() > 0 ? round($this->reviews->sum('star')/$this->reviews->count(),2) : 'No rating yet',
+            'href' => [
+                //php artisan route:list
+                'reviews' => route('reviews.index',$this->id) 
+            ]
         ];
     }
 }
